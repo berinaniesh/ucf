b221c44f7416d90dc3c1eec73c93bad139daedfa
Berin Aniesh <berinaniesh@gmail.com>
2022-11-23 17:33:42 +0530

    reformat

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,32 @@ const PROGRAM_DESC: &'static str = "Universal Code Formatter";
 
 fn print_help() {
     let help_string: String = String::from("Help");
+    println!("{}", help_string);
 }
 
-fn parse() {
-
+fn parse_config() -> () {
+    
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{}", args[1]);
+    dbg!(&args);
     if args.len() != 2 {
         panic!("Improper number of arguements given");
     }
 
-    let _languages = [("C", ".c", "clang-format"), ("C++", ".cpp", "clang-format"), ("C++", ".cc", "clang-format"), ("Java", ".java", "google-java-format"), ("Python", ".py", "black"), ("Javascript", ".js", "prettier"), ("Typescript", "ts", "prettier")];
+    let _languages = [
+        ("C", ".c", "clang-format"),
+        ("C++", ".cpp", "clang-format"),
+        ("C++", ".cc", "clang-format"),
+        ("Java", ".java", "google-java-format"),
+        ("Python", ".py", "black"),
+        ("Javascript", ".js", "prettier"),
+        ("Typescript", "ts", "prettier"),
+    ];
 
     match args[1].as_str() {
-       "--help" => print_help(),
-       _ => {}
+        "--help" => print_help(),
+        _ => {}
     }
-
 }
